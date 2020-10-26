774fe9c6404628a55ba89226dbc74ef62d278298
Ilya Talantsev <italantsev@gotrg.com>
2020-10-26 12:15:40 +0300

    10/26/2020

---
+++ csharp_knowledge_base/LanguageFeatures/VariantGenericInterfaces.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace LanguageFeatures
+{
+    public class CovariantInterface : ICovariantInterface<Derived>
+    {
+        public Base DoSomething()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Derived DoSomething(Action<Derived> action)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+    // Covariance allows interface methods to have more derived return types than that defined by the generic type parameters.
+    public interface ICovariantInterface<out T>
+    {
+        T DoSomething();
+        T DoSomething(Action<T> action);
+    }
+
+    // Contravariance allows interface methods to have argument types that are less derived than that specified by the generic parameters.
+    public interface IContravarianceInterface<in T>
+    {
+        void DoSomething(T t);
+    }
+
+    public class Base
+    {
+        
+    }
+
+    public class Derived : Base
+    {
+        
+    }
+}
